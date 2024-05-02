8b584b82ba4d6a2c56a64916013250d56432a645
john-shoukry <167035378+john-shoukry@users.noreply.github.com>
2024-05-02 23:41:40 +0300

    Update LED_ON.c

--- First module/LED_ON.c
+++ First module/LED_ON.c
@@ -1,17 +1,33 @@
 //John_Shoukry
 
+
+
 #include "tm4c123gh6pm.h"
 #include "SwitchMethods.h"
 #include "LED_ON.h"
 
-void set_led(void){
-	if(Switch2data() == 0){
-		GPIO_PORTF_DATA_R |= BLUE;    			 //turn on blue led 		
+void set_led(char data){
+		GPIO_PORTF_DATA_R |= data;    			 //turn on blue led 		
+	
+}
+
+void clear_led(char data){
+		GPIO_PORTF_DATA_R &=~ data;    //turn off all leds 		
+}
+
+void check_DestinationSet(void)
+{
+	if (Switch2data() == 0)
+	{
+		set_led(BLUE);
 	}
 }
 
-void clear_led(void){
-	if((Switch1data() == 0) && (Switch2data() == 1)){
-		GPIO_PORTF_DATA_R &=~ PF123_mask;    //turn off all leds 		
+void check_DestinationClear(void)
+{
+	if (Switch1data() == 0)
+	{
+		clear_led(BLUE);
 	}
 }
+
