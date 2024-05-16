91422a9c9b958884575d70d44d5a5126719f754a
JohnE03 <167023195+JohnE03@users.noreply.github.com>
2024-05-16 23:33:14 +0300

    Update trajectory_plotter.py

--- Second module/drivers/trajectory_plotter.py
+++ Second module/drivers/trajectory_plotter.py
@@ -12,7 +12,20 @@ while True:
     readedText = readedText.decode("utf-8")
     # Check if reached destination
     if("end" in readedText):
+    print(latitude_list)
+    print(longitude_list)
+    mymap = folium.Map(location=[latitude_list[0], longitude_list[0]], zoom_start=45)
+    start_coords = (latitude_list[0], longitude_list[0])
+    end_coords = (latitude_list[-1], longitude_list[-1])
+    folium.Marker(location=start_coords, popup='Start').add_to(mymap)
+    folium.Marker(location=end_coords, popup='End').add_to(mymap)
+    num_waypoints = len(latitude_list) - 2  # Exclude start and end coordinates
+    for i in range(1, len(latitude_list) - 1):
+        folium.CircleMarker(location=(latitude_list[i], longitude_list[i]), radius=0.2, color='blue').add_to(mymap)
 
+    mymap.save(r'map.html')      
+    break
+    # Parsing lat and lon then store them
   
 
     data = readedText.split(',')
