3337e3bce8e08ca971a269ddb7797b9fc70e9328
Youssef-Ehab30 <167027905+Youssef-Ehab30@users.noreply.github.com>
2024-05-16 23:25:31 +0300

    Add files via upload

---
+++ Second module/main.c
@@ -0,0 +1,83 @@
+//Youssef Ehab
+
+#include "tm4c123gh6pm.h"
+#include <stdio.h>
+#include <stdint.h>
+#include "GPS.h"
+#include "UART.h"
+#include "RGB.h"
+#include "Systick.h"
+#include "LCD.h" 
+#include "Switches.h"
+#include "EEPROM.h"
+#include "Interrupts.h"
+
+
+float prevlat;
+float prevlong;
+float currentlat;
+float currentlong;
+float templat;
+float templong;
+uint8_t hundredMeterFlag=1;	
+
+int main() {
+  double inst_distance=0;
+	double total_distance=0;
+  char LCD_String[20];
+	RGB_init();
+  init_UART0();
+  UART2_init();
+	Switches_init();
+	LCD4bits_Init();
+	UART_Interrupt();
+	setLed(0x02);
+	LCD4bits_Init();
+	EEPROM_Init();
+	
+	LCD_WriteString("Please wait...");
+	LCD4bits_Cmd(0xC0);													//to begin at 2nd line
+	LCD_WriteString("initializing GPS");
+	
+	lineparsing();
+	LCD4bits_Cmd(0x01);													//Clear LCD display
+	LCD4bits_Cmd(0x80);													//to begin at 1st line
+	LCD_WriteString(" click to start ");
+
+	while(s2_read()==0x01);											//Press Switch 2 to start
+	lineparsing();															//get first point from GPS
+	sprintf(LCD_String,"total distance :");
+	LCD4bits_Cmd(0x80);  
+	LCD_WriteString(LCD_String);
+	sprintf(LCD_String,"   %f",total_distance);
+	LCD4bits_Cmd(0xC0);
+	LCD_WriteString(LCD_String);
+	EEPROM_Write(currentlat);
+	delay(2);
+	EEPROM_Write(currentlong);
+	delay(2);
+
+	while(1){
+		delay(100);
+		prevlat=currentlat;
+		prevlong=currentlong;
+		lineparsing();
+		inst_distance=distance(prevlat,prevlong,currentlat,currentlong);
+		EEPROM_Write(currentlat);
+		delay(2);
+		EEPROM_Write(currentlong);
+		delay(2);
+		total_distance+=inst_distance;
+		sprintf(LCD_String,"total distance :");
+		LCD4bits_Cmd(0x01);
+		LCD_WriteString(LCD_String);
+		sprintf(LCD_String,"   %f",total_distance);
+		LCD4bits_Cmd(0xC0);
+		LCD_WriteString(LCD_String);
+		
+		if (total_distance>100 && hundredMeterFlag==1){
+			hundred_meter_Handler();
+			hundredMeterFlag=0;
+		}
+	}
+}
